eb4dda8facc56c9dbd3f09e22dc0b1e5ad6219cd
Kaushik <kaushikkislay.kk@gmail.com>
2017-11-13 03:45:37 +0530

    Intent calls added

--- loggerutil/src/main/java/com/lib/kaushikkislay/loggerutil/myLogger.java
+++ loggerutil/src/main/java/com/lib/kaushikkislay/loggerutil/myLogger.java
@@ -15,7 +15,7 @@ import android.hardware.SensorEvent;
  * Created by kaushikkislay on 12/11/17.
  */
 
-public class myLogger extends IntentService implements SensorEventListener {
+public class myLogger extends IntentService {
 
     private SensorManager SM;
     private Sensor sn;
@@ -36,7 +36,7 @@ public class myLogger extends IntentService implements SensorEventListener {
         Toast.makeText(this,"Service started", Toast.LENGTH_LONG).show();
         SM = (SensorManager)getSystemService(SENSOR_SERVICE);
         sn= SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        SM.registerListener(this, sn, SensorManager.SENSOR_DELAY_NORMAL);
+        SM.registerListener((SensorEventListener) this, sn, SensorManager.SENSOR_DELAY_NORMAL);
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -52,6 +52,20 @@ public class myLogger extends IntentService implements SensorEventListener {
     protected void onHandleIntent(@Nullable Intent intent) {
 
         synchronized (this){
+            SensorEventListener sel = new SensorEventListener(){
+
+                @Override
+                public void onSensorChanged(SensorEvent event) {
+                    Log.d("TAG X: ", String.valueOf(event.values[0]));
+                    Log.d("TAG Y: ", String.valueOf(event.values[1]));
+                    Log.d("TAG Z: ", String.valueOf(event.values[2]));
+                }
+
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+                }
+            };
             for (int i=0;i<20;i++){
                 Log.d("TAG", Integer.toString(i));
                 try {
@@ -63,15 +77,7 @@ public class myLogger extends IntentService implements SensorEventListener {
         }
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        Log.d("TAG X: ", String.valueOf(event.values[0]));
-        Log.d("TAG Y: ", String.valueOf(event.values[1]));
-        Log.d("TAG Z: ", String.valueOf(event.values[2]));
-    }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-    }
+
 }
