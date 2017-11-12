be96083bb31527eeea19bd9a9b95ae6dd97d2230
Kaushik <kaushikkislay.kk@gmail.com>
2017-11-13 01:56:33 +0530

    Intent calls added

---
+++ loggerutil/src/main/java/com/lib/kaushikkislay/loggerutil/intentcaller.java
@@ -0,0 +1,18 @@
+package com.lib.kaushikkislay.loggerutil;
+
+import android.content.Intent;
+import android.util.Log;
+
+
+/**
+ * Created by kaushikkislay on 13/11/17.
+ */
+
+public class intentcaller {
+    private static final String TAG = "AWESOME_LOGGER";
+    public static void mylog (String str, myLogger ctx) {
+        Log.d(TAG, str);
+        Intent myintent = new Intent(ctx,myLogger.class);
+        ctx.startService(myintent);
+    }
+}
--- loggerutil/src/main/java/com/lib/kaushikkislay/loggerutil/myLogger.java
+++ loggerutil/src/main/java/com/lib/kaushikkislay/loggerutil/myLogger.java
@@ -1,15 +1,56 @@
 package com.lib.kaushikkislay.loggerutil;
 
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by kaushikkislay on 12/11/17.
  */
 
-public class myLogger {
+public class myLogger extends IntentService {
 
-    private static final String TAG = "AWESOME_LOGGER";
-    public static void mylog (String str){
-        Log.d(TAG,str);
+
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public myLogger() {
+        super("Worker thread");
+    }
+
+
+
+    @Override
+    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
+        Toast.makeText(this,"Service started", Toast.LENGTH_LONG).show();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"Service stopped", Toast.LENGTH_LONG).show();
+
+
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+        synchronized (this){
+            for (int i=0;1<10;i++){
+                Log.d("TAG", Integer.toString(i));
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
