ffc9744fe91377bb9f51d08cac50750749c724b3
Kaushik <kaushikkislay.kk@gmail.com>
2017-11-13 02:57:32 +0530

    Intent calls added

--- loggerutil/src/main/java/com/lib/kaushikkislay/loggerutil/myLogger.java
+++ loggerutil/src/main/java/com/lib/kaushikkislay/loggerutil/myLogger.java
@@ -6,13 +6,19 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorEvent;
+
 /**
  * Created by kaushikkislay on 12/11/17.
  */
 
-public class myLogger extends IntentService {
+public class myLogger extends IntentService implements SensorEventListener {
 
-
+    private SensorManager SM;
+    private Sensor sn;
 
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
@@ -28,6 +34,9 @@ public class myLogger extends IntentService {
     @Override
     public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
         Toast.makeText(this,"Service started", Toast.LENGTH_LONG).show();
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sn= SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        SM.registerListener(this, sn, SensorManager.SENSOR_DELAY_NORMAL);
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -52,5 +61,17 @@ public class myLogger extends IntentService {
                 }
             }
         }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.d("TAG X: ", String.valueOf(event.values[0]));
+        Log.d("TAG Y: ", String.valueOf(event.values[1]));
+        Log.d("TAG Z: ", String.valueOf(event.values[2]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
